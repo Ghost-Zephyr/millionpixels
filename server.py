75f929901678cb630d4d03d0658311b0bb7a477a
Sivert V. Sæther <sivert.s13@gmail.com>
2020-06-03 12:57:26 +0200

    skool

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import routes
 jwt.keypair = jwt.set_keypair(jwt.read_keyfiles())
 
 app = Flask(__name__)
-#app.jinja_env = Environment(loader=FileSystemLoader('pugs'), trim_blocks=True) TODO: to rename templates folder? from jinja2 import Environment, FileSystemLoader
+#app.jinja_env = Environment(loader=FileSystemLoader('pugs'), trim_blocks=True) # TODO: to rename templates folder? from jinja2 import Environment, FileSystemLoader
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 
 app.config['MONGO_DBNAME'] = 'million'
